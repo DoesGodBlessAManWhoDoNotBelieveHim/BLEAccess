446b33677c84c9842dbf8a7feb3eb31344dba293
Jing Zhang <king91chang@163.com>
2015-10-10 11:26:03 +0800

    init
    新建工程蓝牙门禁，用swift语言来实现

---
+++ BLEAccess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEAccess
+//
+//  Created by wrt on 15/10/10.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
